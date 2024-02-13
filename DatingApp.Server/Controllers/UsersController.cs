084103b517c13424bbe10b9d600b75e5aed7d1de
Leo7962 <IngenieroLeonardo@outlook.com>
2024-02-13 15:50:19 -0500

    Realizando métodos asincronos y también actualización de librerías.

--- DatingApp.Server/Controllers/UsersController.cs
+++ DatingApp.Server/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
 ﻿using DatingApp.Server.Data;
+using DatingApp.Server.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.Server.Controllers
 {
@@ -12,6 +14,20 @@ namespace DatingApp.Server.Controllers
         public UsersController(DataContext context)
         {
             this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await context.Users.FindAsync(id);
         }
     }
 }
